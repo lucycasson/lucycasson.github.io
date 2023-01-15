8af655b43ab73c847136f22a206bfe63d496cb7a
Ryan <ryan.lawrence.business@gmail.com>
2023-01-15 13:15:04 +1100

    image load wait implemented on all content
    Increases priority of wrong height flash on load

--- main.js
+++ main.js
@@ -26,18 +26,20 @@ $(document).ready(function () {
   });
 
   // Wait until images load to do page height animation
-  var $grid = $(".grid").imagesLoaded(function () {
-    let height = $("#container").height(); // Get auto height of container [CAN WE CALCULATE THIS WITHOUT SETTING IT?]
-    $("#container").css("height", localStorage.getItem("startingHeight"));
-    // Set height to stored height (from prev page)
-    $("#container").animate(
-      // Animate height to auto height
-      { height: height },
-      1000
-    );
-    localStorage.setItem("startingHeight", height); // Store height for next page
-    // TODO: fix auto height of container on window resize WITHOUT RELOADING PAGE
-  });
+  $("#container")
+    .imagesLoaded()
+    .always(function () {
+      let height = $("#container").height(); // Get auto height of container [CAN WE CALCULATE THIS WITHOUT SETTING IT?]
+      $("#container").css("height", localStorage.getItem("startingHeight"));
+      // Set height to stored height (from prev page)
+      $("#container").animate(
+        // Animate height to auto height
+        { height: height },
+        1000
+      );
+      localStorage.setItem("startingHeight", height); // Store height for next page
+      // TODO: fix auto height of container on window resize WITHOUT RELOADING PAGE
+    });
 });
 
 onbeforeunload = function () {
