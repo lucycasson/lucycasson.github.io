f544264a4b630cf2fd5f7e881011cf670edb76c0
Ryan <ryan.lawrence.business@gmail.com>
2023-01-15 15:31:20 +1100

    remove remaining animation code

--- main.js
+++ main.js
@@ -6,35 +6,6 @@ $(document).ready(function () {
     300
   );
 
-  // Wait until images load to do page height animation
-  $("#container")
-    .imagesLoaded()
-    .always(function () {
-      var $originalDiv = $("#container");
-      var $cloneDiv = $originalDiv.clone();
-      $cloneDiv.css("visibility", "hidden");
-      $("body").append($cloneDiv);
-
-      $cloneDiv.height("auto");
-      var newHeight = $cloneDiv.height();
-      console.log($cloneDiv.height());
-
-      $cloneDiv.remove();
-
-      $("#container").animate(
-        // Animate height to auto height
-        { height: newHeight },
-        1000
-      );
-
-      localStorage.setItem("startingHeight", newHeight); // Store height for next page
-    });
-
-  $(window).resize(function () {
-    newHeight = $("#container").height("auto").height();
-    localStorage.setItem("startingHeight", newHeight); // Store height for next page
-  });
-
   // after submit button is clicked, redirect to the next page
   $("#submit").click(function () {
     // wait for 1 second before redirecting
