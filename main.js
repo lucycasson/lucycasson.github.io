70ef07589c8a8a350aaca1ea0019e726860fc058
Ryan <ryan.lawrence.business@gmail.com>
2024-04-16 17:57:15 +1000

    reduce vibration length

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ $(document).ready(function () {
   function provideHapticFeedback() {
     // Check if the Vibration API is supported by the browser
     if ("vibrate" in navigator) {
-      // Vibrate for 100 milliseconds
-      navigator.vibrate(75);
+      // Vibrate for 50 milliseconds
+      navigator.vibrate(50);
     } else {
       // Fallback for browsers that don't support the Vibration API
       console.log("Vibration API not supported");
